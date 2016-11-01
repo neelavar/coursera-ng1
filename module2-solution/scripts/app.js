78c20a450feba53fccde35ef7047fa20f9157980
Prasanna Neelavar <prasanna.neelavar@gmail.com>
2016-11-01 23:55:25 +0530

    updated module2-solution with service and two controllers

--- module2-solution/scripts/app.js
+++ module2-solution/scripts/app.js
@@ -4,19 +4,23 @@
     'use strict';
 
     angular.module('CheckOffApp', [])
-        .controller('CheckOffController', CheckOffController);
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    //    CheckOffController.$inject = ['$scope'];
+    // Inject the service into the controllers
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
-    //Controller function
-    function CheckOffController() {
-        var shoppingList = this;
+    // Service function
+    function ShoppingListCheckOffService() {
+        var service = this;
 
         // toBuyItems - array holds the data for shopping list items to buy
-        shoppingList.toBuyItems = [
+        var toBuyItems = [
             {
                 name: 'cookies',
-                quantity: 10
+                quantity: 25
             }, {
                 name: 'chips',
                 quantity: 5
@@ -30,22 +34,53 @@
                 name: 'donuts',
                 quantity: 12
             }
-        ]; // loaded with 5 items
+        ]; // loaded with 5 items into toBuy List
 
         // boughtItems - array holds the data for shopping list items that are bought
-        shoppingList.boughtItems = [
-
-        ]; // initialized with no items to start with
+        var boughtItems = []; // initialized with no items to start with
 
         // checkOff() - function to remove the item from toBuyItems and
         // add the same item into boughtItems array
-        shoppingList.checkOff = function (index) {
+        service.checkOff = function (index) {
             // momentarily hold the 'item' on which checkOff is called for
-            var item = shoppingList.toBuyItems[index];
-            shoppingList.toBuyItems.splice(index, 1); // remove the item from toBuyItems
-            shoppingList.boughtItems.push(item); // add into boughtItems
+            var item = toBuyItems[index];
+            toBuyItems.splice(index, 1); // remove the item from toBuyItems
+            boughtItems.push(item); // add into boughtItems
         };
-    };
 
+        // function to expose toBuyItems array
+        service.getToBuyItems = function () {
+            return toBuyItems;
+        };
+
+        // function to expose boughtItems array
+        service.getAlreadyBoughtItems = function () {
+            return boughtItems;
+        };
+
+    }; // end of service function
+
+    // ToBuyContoller function
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuy = this;
+
+        // obtain the items list from the service
+        toBuy.itemList = ShoppingListCheckOffService.getToBuyItems();
+
+        // utilize the checkOff function from the service
+        toBuy.checkOff = function (index) {
+            return ShoppingListCheckOffService.checkOff(index);
+        };
+
+    }; // end of ToBuyController
+
+    // AlreadyBoughtController function
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var alreadyBought = this;
+
+        // obtain the items list from the service
+        alreadyBought.itemList = ShoppingListCheckOffService.getAlreadyBoughtItems();
+
+    }; // end of AlreadyBoughtController
 
 })();
