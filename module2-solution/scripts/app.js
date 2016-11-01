75b99d16763130545e6953b33f1f0138c203b133
Prasanna Neelavar <prasanna.neelavar@gmail.com>
2016-11-01 23:09:38 +0530

    updated with module2-solution

--- module2-solution/scripts/app.js
+++ module2-solution/scripts/app.js
@@ -1,54 +1,51 @@
 //./app.js
 //Main app js file to define angular module and controller
-(function(){
-  'use strict';
+(function () {
+    'use strict';
 
-  angular.module('LunchChecker',[])
-        .controller('LunchCheckController', LunchCheckController)
-        .$inject = ['$scope'];
+    angular.module('CheckOffApp', [])
+        .controller('CheckOffController', CheckOffController);
 
-//Controller function
-  function LunchCheckController($scope){
-    $scope.menuItems = ""; // holds the data for menu items entered by the user
-    $scope.response = ""; // holds the response message
-    $scope.alertState = ""; // holds the alert state to style the response message
-    $scope.icon = ""; // glyphicon icon to associate with the response message
+    //    CheckOffController.$inject = ['$scope'];
 
-//function to split the lunch menu text and count the no. of items
-    function getMenuItemsCount(string){
-      var itemCount = 0;
-      var stringArray = string.split(',');
-      for (var i = 0; i < stringArray.length; i++) {
-        stringArray[i] = stringArray[i].replace(/ /g,'');
-        if(stringArray[i].length >0) itemCount++;
-      }
-      return itemCount;
+    //Controller function
+    function CheckOffController() {
+        var shoppingList = this;
+
+        // toBuyItems - array holds the data for shopping list items to buy
+        shoppingList.toBuyItems = [
+            {
+                name: 'cookies',
+                quantity: 10
+            }, {
+                name: 'chips',
+                quantity: 5
+            }, {
+                name: 'soda',
+                quantity: 10
+            }, {
+                name: 'cheese blocks',
+                quantity: 4
+            }, {
+                name: 'donuts',
+                quantity: 12
+            }
+        ]; // loaded with 5 items
+
+        // boughtItems - array holds the data for shopping list items that are bought
+        shoppingList.boughtItems = [
+
+        ]; // initialized with no items to start with
+
+        // checkOff() - function to remove the item from toBuyItems and
+        // add the same item into boughtItems array
+        shoppingList.checkOff = function (index) {
+            // momentarily hold the 'item' on which checkOff is called for
+            var item = shoppingList.toBuyItems[index];
+            shoppingList.toBuyItems.splice(index, 1); // remove the item from toBuyItems
+            shoppingList.boughtItems.push(item); // add into boughtItems
+        };
     };
 
-//function to set the response message based on the menu item count
-// if no items entered (0) - case 0
-// for any values between 1 & 3 (inclusive) - Enjoy!
-// otherwise - for values more than 3 - Too much!
-     $scope.checkIfTooMuch = function(){
-       switch (getMenuItemsCount($scope.menuItems)){
-         case 0:
-           $scope.response = 'Please enter data first';
-           $scope.alertState = 'danger';
-           $scope.icon = 'exclamation-sign';
-           break;
-         case 1:
-         case 2:
-         case 3:
-           $scope.response = 'Enjoy!';
-           $scope.alertState = 'success';
-           $scope.icon = 'ok';
-           break;
-         default:
-           $scope.response = 'Too much!';
-           $scope.alertState = 'warning';
-           $scope.icon = 'flag';
-       }
-    };
 
-  };
 })();
